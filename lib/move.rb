8cbee9ef4017b6b2812aaeec12a0bf95633cf2e0
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-08 16:56:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,6 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def input_to_index(user_input)
   return user_input.to_i - 1
 end
