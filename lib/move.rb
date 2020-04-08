042d7c0563530e8b5b45fb999cb07dc6d74d6357
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-08 16:17:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,3 +14,10 @@ end
 def move(board,user_input,character='X')
  return "#{board[0]}"
 end
+
+board = [" ", " ", " "]
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+
+update_array_at_with(board, 0, "X")
